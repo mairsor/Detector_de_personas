d0309231156af48872971da0f15f67be19ab3b2c
Gabriel Marcelo Ferre Castro <gabriel.ferre.c@uni.pe>
2023-04-27 16:13:44 -0500

    Comentarios añadidos
    Añadí comentarios para explicar las funciones de las líneas de comando y cambiié la línea 14, cambiando el 4 de digitalRead(4) por digitalRead(pinEntradaDetector) para que se haga más entendible.

--- Detectorpersonas/Detectorpersonas.ino
+++ Detectorpersonas/Detectorpersonas.ino
@@ -1,31 +1,28 @@
-int presencia, i = 0;
+int presencia;
 int pinSalidaLED = 9;
 int pinEntradaDetector = 4;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(pinEntradaDetector, INPUT);
-  pinMode(pinSalidaLED, OUTPUT);
+  pinMode(pinEntradaDetector, INPUT); //El pinEntradaDetector será el pin por el cual ingresará la señal del PIR.
+  pinMode(pinSalidaLED, OUTPUT); //El pinSalidaLED será el pin por el cual saldrá la señal para activar el LED o no.
 
 }
 
 void loop() {
-  presencia = digitalRead(4);
-  Serial.print(presencia);
-  delay(50);
+  presencia = digitalRead(pinEntradaDetector); //Dato que ingresa del PIR. Si es 1 hay movimiento, si es 0 no hay movimiento.
+  
+  delay(100);
 
   if(presencia==1){
-    
-    digitalWrite(pinSalidaLED, 1);
-    
-    
+    digitalWrite(pinSalidaLED, 1); //Si el PIR detecta movimiento, mandará la señal 1 y se prenderá el led.
+    Serial.print("\nHAY MOVIMIENTO");
     }
     
   else{
-    
-      digitalWrite(pinSalidaLED, 0);
-      
+      digitalWrite(pinSalidaLED, 0); //SI el PIR no detecta movimiento, mandará la señal 0 y no se prenderá el LED.
+      Serial.print("\nQUIETO");
     }
   }
 
